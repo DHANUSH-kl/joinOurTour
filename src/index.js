bd06f7e67eb91ace9d7945b1800e31186b7ab4e7
Dhanush KL <dhanuchandu1232@gmail.com>
2024-02-14 00:45:35 +0530

    testing index route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv';
+import express  from 'express';
 import connectDB from './db/db.js'
 import {app} from './app.js'
+import methodOverride from 'method-override';
+import ejsMate from 'ejs-mate';
+import { urlencoded } from 'express';
+import path from 'path'; 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config({
     path:'./env'
@@ -15,4 +25,27 @@ connectDB()
 } )
 .catch((error)=>{
     console.log( "mongoDB connection error" , error );
+})
+
+
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname , "/public")));
+app.set("view engine" , "ejs");
+app.set("views" , path.join(__dirname , "views" ));
+app.engine("ejs",ejsMate);
+
+
+app.get("/",(req,res)=> {
+    res.render("admin/newTrip.ejs")
+})
+
+app.post("/",(req,res) => {
+    let {departure,tripTitle,totalDays,startLocation,tripDescription,} = req.body;
+    console.log(departure);
+    console.log(tripTitle);
+    console.log(totalDays);
+    console.log(startLocation);
+    console.log(tripDescription);
+    res.redirect("/");
 })
--- src/models/travel.model.js
+++ src/models/travel.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const tripSchema = new mongoose.Schema({
-    departureDate : {
+    departure : {
         type : String,
         required : true
     },
