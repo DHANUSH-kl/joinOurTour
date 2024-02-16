03c21c82b671a574f2bf1c9329dfd2b4c969cab8
Dhanush KL <dhanuchandu1232@gmail.com>
2024-02-16 17:24:51 +0530

    added includes

--- src/controllers/newTrip.controler.js
+++ src/controllers/newTrip.controler.js
@@ -1,14 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+// Use body-parser middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 const showNewTrip = (req,res) => {
     res.render("admin/newTrip.ejs")
 }
 
 const addNewTrip = (req,res)=>{
-    let {departure,tripTitle,totalDays,startLocation,tripDescription,} = req.body;
+    let {departure,catagaries,title,description,accomodations,aboutLeader,includes} = req.body;
     console.log(departure);
-    console.log(tripTitle);
-    console.log(totalDays);
-    console.log(startLocation);
-    console.log(tripDescription);
+    console.log(catagaries);
+    console.log(title);
+    console.log(description);
+    console.log(aboutLeader);
+    console.log(accomodations);
+    console.log(includes);
     res.redirect("/");
 }
 
